89d5448c2f31707a89160c25d014d5e66b910666
Weslley-Prado <weslley.prado73@gmail.com>
2022-10-23 19:15:35 -0300

    Menu

---
+++ tigersystem/src/components/nav/Nav.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import Logo from '../../assets/img/logo.png';
+import './Nav.css'
+
+export function Nav() {
+    return(
+       <div className="menu-area">
+            <nav className="navbar navbar-expand-lg fixed-top navbar-dark menu" >
+
+            <Link to="/" className="logo navbar-brand">
+              <img className="logo" src = {Logo} alt="logo" />
+           </Link>
+               <div className="linkMenu">
+                <Link className="link" to={"/"}>Home</Link>
+                <Link className="link" to={"/"}>About</Link>
+                <Link className="link" to={"/"}>Product</Link>
+                <Link className="link" to={"/"}>Services</Link>
+                <Link className="link" to={"/"}>Contact</Link>
+            </div>
+            </nav>
+         </div>
+    )
+}
