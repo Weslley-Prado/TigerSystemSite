956406f6f868334a18e32cfc3c5618a235a0e98e
Weslley-Prado <weslley.prado73@gmail.com>
2022-11-02 14:42:08 -0300

    Feat(MenuResponsive): Creating menu responsive

--- tigersystem/src/App.tsx
+++ tigersystem/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import "./App.css";
 import { Nav } from "./components/nav/Nav";
 import { BrowserRouter } from "react-router-dom";
--- tigersystem/src/components/nav/Nav.tsx
+++ tigersystem/src/components/nav/Nav.tsx
@@ -1,24 +1,38 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import Logo from '../../assets/img/logo.png';
-import './Nav.css'
+import { Link } from "react-router-dom";
+import Logo from "../../assets/img/logo.png";
+import "./Nav.css";
 
 export function Nav() {
-    return(
-       <div className="menu-area">
-            <nav className="navbar navbar-expand-lg fixed-top navbar-dark menu" >
-
-            <Link to="/" className="logo navbar-brand">
-              <img className="logo" src = {Logo} alt="logo" />
-           </Link>
-               <div className="linkMenu">
-                <Link className="link" to={"/"}>Home</Link>
-                <Link className="link" to={"/"}>About</Link>
-                <Link className="link" to={"/"}>Product</Link>
-                <Link className="link" to={"/"}>Services</Link>
-                <Link className="link" to={"/"}>Contact</Link>
-            </div>
-            </nav>
-         </div>
-    )
+     
+   
+  return (
+    <div className="menu-area">
+      <nav className="navbar navbar-expand-lg fixed-top navbar-dark menu">
+        <Link to="/" className="logo navbar-brand">
+          <img className="logo" src={Logo} alt="logo" />
+        </Link>
+        <div className="hamburger">
+        <img src="https://img.icons8.com/fluency/48/000000/menu--v1.png"/>
+        </div>
+        <div className="linkMenu">
+          <Link className="link" to={"/"}>
+            Home
+          </Link>
+          <Link className="link" to={"/"}>
+            About
+          </Link>
+          <Link className="link" to={"/"}>
+            Product
+          </Link>
+          <Link className="link" to={"/"}>
+            Services
+          </Link>
+          <Link className="link" to={"/"}>
+            Contact
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
