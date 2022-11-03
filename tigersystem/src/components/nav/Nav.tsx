ca87ed2d596951c06adb164f70f44755b4f4b7b4
Weslley-Prado <weslley.prado73@gmail.com>
2022-11-02 23:34:32 -0300

    Update

--- tigersystem/src/components/nav/Nav.tsx
+++ tigersystem/src/components/nav/Nav.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/img/logo.png";
 import "./Nav.css";
 
 export function Nav() {
-     
-   
+  const [isNavExpanded, setNavExpanded] = useState(false);
+
+  const handleExpanded = useCallback(() => {
+    if (!isNavExpanded) {
+      alert(isNavExpanded);
+      setNavExpanded(isNavExpanded);
+      console.log(isNavExpanded);
+    }
+  }, [isNavExpanded]);
+
   return (
     <div className="menu-area">
       <nav className="navbar navbar-expand-lg fixed-top navbar-dark menu">
@@ -13,7 +21,28 @@ export function Nav() {
           <img className="logo" src={Logo} alt="logo" />
         </Link>
         <div className="hamburger">
-        <img src="https://img.icons8.com/fluency/48/000000/menu--v1.png"/>
+          <div>
+            <img
+              onClick={handleExpanded}
+              src="https://img.icons8.com/fluency/48/000000/menu--v1.png"
+            />
+          </div>
+          <div className="menuResponsive">
+            <div className="linkResp">
+              <Link className="linkMenResp" to={"/"}>
+                Home <br />
+              </Link>
+              <Link className="linkMenResp" to={"/"}>
+                About <br />
+              </Link>
+              <Link className="linkMenResp" to={"/"}>
+                Service <br />
+              </Link>
+              <Link className="linkMenResp" to={"/"}>
+                Contact <br />
+              </Link>
+            </div>
+          </div>
         </div>
         <div className="linkMenu">
           <Link className="link" to={"/"}>
