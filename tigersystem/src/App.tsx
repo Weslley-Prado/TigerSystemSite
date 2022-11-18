ee300d2bcc608555b169174d34a940d1d78f9f99
Weslley-Prado <weslley.prado73@gmail.com>
2022-11-18 18:45:28 -0300

    Feat(footer): Start

--- tigersystem/src/App.tsx
+++ tigersystem/src/App.tsx
@@ -5,12 +5,14 @@ import { Nav } from "./components/Nav/Nav";
 import { BrowserRouter } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 import Router from "./Route";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
      <Nav />
       <Router />
+      <Footer />
     </BrowserRouter>
   );
 }
