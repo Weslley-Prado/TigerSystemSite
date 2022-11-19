04dd83c85336afea1b6edfcb4c7ff800d6b60b0a
Weslley-Prado <weslley.prado73@gmail.com>
2022-11-18 22:15:42 -0300

    Feat(footer): Start

--- tigersystem/src/components/Footer/Footer.tsx
+++ tigersystem/src/components/Footer/Footer.tsx
@@ -1,124 +1,137 @@
-import React from 'react';
-import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
-import "./Footer.css"
+import React from "react";
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import "./Footer.css";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/img/logo.png";
 
-export  function Footer() {
+export function Footer() {
   return (
-    <MDBFooter bgColor='dark' className='text-center text-lg-start text-muted'>
-      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-        <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks:</span>
+    <MDBFooter bgColor="dark" className="text-center text-lg-start text-muted">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="me-5 d-none d-lg-block">
+          <span>Conecte-se conosco através das nossas redes sociais:</span>
         </div>
 
         <div>
-          <a href='' className='me-4 text-reset'> 
-            <MDBIcon color='primary' fab icon='facebook-f' />
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="primary" fab icon="facebook-f" />
           </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='twitter' />
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="twitter" />
           </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='google' />
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="google" />
           </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='instagram' />
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="instagram" />
           </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='linkedin' />
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="linkedin" />
           </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='github' />
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="github" />
           </a>
         </div>
       </section>
 
-      <section className=''>
-        <MDBContainer className='text-center text-md-start mt-5'>
-          <MDBRow className='mt-3'>
-            <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon color='secondary' icon='gem' className='me-3' />
-                Company name
-              </h6>
+      <section className="">
+        <MDBContainer className="text-center text-md-start mt-5">
+          <MDBRow className="mt-3">
+            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+              <Link to="/" className="logo ">
+                <img className="logo" src={Logo} alt="logo" />
+              </Link>
               <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit
-                amet, consectetur adipisicing elit.
+               Nossa empresa fica feliz com a sua visita ao nosso site.
+               Estamos aqui prontos para para melhor atender as necessidades
+               de nossos clientes.
               </p>
             </MDBCol>
 
-            <MDBCol md='2' lg='2' xl='2' className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Produtos</h6>
               <p>
-                <a href='#!' className='text-reset'>
-                  Angular
+                <a href="#!" className="text-reset">
+                  Sistema de Comanda
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  React
+                <a href="#!" className="text-reset">
+                  Sistema de Vendas
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Vue
+                <a href="#!" className="text-reset">
+                  Ponto Eletrônico
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Laravel
-                </a>
-              </p>
-            </MDBCol>
-
-            <MDBCol md='3' lg='2' xl='2' className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Help
+                <a href="#!" className="text-reset">
+                  Emissor de Nota Fiscal
                 </a>
               </p>
             </MDBCol>
 
-            <MDBCol md='4' lg='3' xl='3' className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+            <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Serviços</h6>
               <p>
-                <MDBIcon color='secondary' icon='home' className='me-2' />
-                New York, NY 10012, US
+                <a href="#!" className="text-reset">
+                  Criação de sites
+                </a>
               </p>
               <p>
-                <MDBIcon color='secondary' icon='envelope' className='me-3' />
-                info@example.com
+                <a href="#!" className="text-reset">
+                  Criação de Ecommerce
+                </a>
               </p>
               <p>
-                <MDBIcon color='secondary' icon='phone' className='me-3' /> + 01 234 567 88
+                <a href="#!" className="text-reset">
+                  Controle de servidores
+                </a>
               </p>
               <p>
-                <MDBIcon color='secondary' icon='print' className='me-3' /> + 01 234 567 89
+                <a href="#!" className="text-reset">
+                  Controle de Backup
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Contato</h6>
+              <p>
+                <MDBIcon color="secondary" icon="home" className="me-2" />
+                Linhares, ES 29900-034, Brasil
+              </p>
+              <p>
+                <MDBIcon color="secondary" icon="envelope" className="me-3" />
+               atendimento@tigersystem.com
+              </p>
+              <p>
+                <MDBIcon color="secondary" icon="phone" className="me-3" /> + 55
+                28 9 9921 2075
+              </p>
+              <p>
+                <MDBIcon color="secondary" icon="phone" className="me-3" /> + 55
+                27 9 9760 0502
               </p>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
       </section>
 
-      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-        © 2020 Copyright: 
-        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-           Tiger System - www.tigersystem.com
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+      >
+        © 2020 Copyright:
+        <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
+          Tiger System - www.tigersystem.com
         </a>
       </div>
     </MDBFooter>
