3da545d9d51b9164bc5765bd87738a8aa59ad6fc
Weslley-Prado <weslley.prado73@gmail.com>
2022-11-23 23:45:56 -0300

    Feat(Product)

--- tigersystem/src/components/Product/Product.tsx
+++ tigersystem/src/components/Product/Product.tsx
@@ -1,25 +1,44 @@
 import React from "react";
 import "./Product.css";
-
+import TigerSystemOrder from "../../assets/img/about.jpg";
 export function Product() {
   return (
     <div>
-      <div>Titulo</div>
-      <section>
-        <div>IMG</div>
-        <div>Texto</div>
+      <div className="titleProduct">
+        <h1 className="title">Sistemas perfeitos para seu negócio</h1>
+        <hr />
+      </div>
+      <section className="contentRight">
+        <div className="imgProductLeft">
+          <img src={TigerSystemOrder} />
+        </div>
+        <div className="textRight">
+          <p>Texto</p>
+        </div>
       </section>
-      <section>
-        <div>Texto</div>
-        <div>IMG</div>
+      <section className="contentleft">
+        <div className="textLeft">
+          <p>Texto</p>
+        </div>
+        <div className="imgProductRight">
+          <img src={TigerSystemOrder} />
+        </div>
       </section>
-      <section>
-        <div>IMG</div>
-        <div>Texto</div>
+      <section className="contentRight">
+        <div className="imgProductLeft">
+          <img src={TigerSystemOrder} />
+        </div>
+        <div className="textRight">
+          <p>Texto</p>
+        </div>
       </section>
-      <section>
-        <div>Texto</div>
-        <div>IMG</div>
+      <section className="contentleft">
+        <div className="textLeft">
+          <p>Texto</p>
+        </div>
+        <div className="imgProductRight">
+          <img src={TigerSystemOrder} />
+        </div>
       </section>
     </div>
   );
