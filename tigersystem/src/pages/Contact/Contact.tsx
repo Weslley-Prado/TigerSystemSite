652a1ab0d40f11d2ffde3dff8c5d983e46373ed1
Weslley-Prado <weslley.prado73@gmail.com>
2022-12-05 22:23:16 -0300

    Feat(form): created form of contact

--- tigersystem/src/pages/Contact/Contact.tsx
+++ tigersystem/src/pages/Contact/Contact.tsx
@@ -1,15 +1,54 @@
-import React from "react"
+import React from "react";
+import "./Contact.css";
 
-export  function Contact() {
-   return(
+export function Contact() {
+  return (
     <form>
-        <div>
-            <input type="text" name="nome" placeholder="Digite seu nome completo" required={true} />
-            <input type="text" name="email" placeholder="Digite seu email" required/>
-            <input type="phone" name="telefone" placeholder="Digite celular" required/>
-            <input type="number" name="document" placeholder="CPF ou CNPJ" required/>
-            <textarea name="require" placeholder="Escreva sua mensagem"></textarea>
+      <div className="formContent">
+        <div className="inputContent">
+          <label>Nome *</label>
+          <div>
+            <input
+              type="text"
+              name="nome"
+              placeholder="Digite seu nome completo"
+              required={true}
+            />
+          </div>
+          <label>Email *</label>
+          <div>
+            <input
+              type="text"
+              name="email"
+              placeholder="Digite seu email"
+              required
+            />
+          </div>
+          <label>Celular *</label>
+          <div>
+            <input
+              type="phone"
+              name="telefone"
+              placeholder="Digite celular"
+              required
+            />
+          </div>
+          <label>CNPJ/CPF</label>
+          <div>
+            <input
+              type="number"
+              name="document"
+              placeholder="CPF ou CNPJ"
+              required
+            />
+          </div>
+          <label>Mensagem</label>
+          <div>
+            <textarea name="require" placeholder="Escreva sua mensagem" />
+          </div>
+          <button className="formButton">Enviar</button>
         </div>
+      </div>
     </form>
-   )
+  );
 }
