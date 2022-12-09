7148f4146992e7cedcfd2ec72bfe13cd17e77467
Weslley-Prado <weslley.prado73@gmail.com>
2022-12-09 17:54:23 -0300

    feat: create project api

---
+++ tigersystemseller-api/src/main/java/com/tigersystemseller/TigersystemsellerApiApplication.java
@@ -0,0 +1,13 @@
+package com.tigersystemseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigersystemsellerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigersystemsellerApiApplication.class, args);
+	}
+
+}
