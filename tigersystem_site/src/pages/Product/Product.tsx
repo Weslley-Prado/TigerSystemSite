c2d95af57c58c9380af2de5c14d4dd19e163a1b2
Weslley-Prado <weslley.prado73@gmail.com>
2022-12-20 17:02:35 -0300

    feat: finish product page

--- tigersystem_site/src/pages/Product/Product.tsx
+++ tigersystem_site/src/pages/Product/Product.tsx
@@ -13,22 +13,31 @@ export function Product() {
           <img src={TigerSystemOrder} />
         </div>
         <div className="textRight">
-          <h3>Tiger System - Order</h3>
+          <h3>Tiger System - Gourmet</h3>
           <hr />
-          <p>Texto</p>
+          <p className="lead">O sistema Tiger System Gourmet proporciona uma experiência única para seu restaurante. Com ele é possível
+            que o garçom faça o pedido e ele seja enviado para a cozinha sem o garçom precisar ir lá.
+            Isso melhora a qualidade do atendimento aos seus clientes. <br />
+            <button className="buttonProduct">Saiba mais</button>
+          </p>       
         </div>
       </section>
       <section className="contentleft">
         <div className="textLeft">
         <h3>Tiger System - Seller</h3>
           <hr />
-          <p>Texto</p>
+          <p className="lead">
+            O Tiger System Seller proporciona uma melhor experiência no seu comércio
+            ao realizar vendas. Ele atende muito bem comércios em geral, como autopeças,
+            depósitos, casas de ração e muitos outros. <br />
+            <button className="buttonProduct">Saiba mais</button>
+          </p>
         </div>
         <div className="imgProductRight">
           <img src={TigerSystemOrder} />
         </div>
       </section>
-      <section className="contentRight">
+      {/* <section className="contentRight">
         <div className="imgProductLeft">
           <img src={TigerSystemOrder} />
         </div>
@@ -47,7 +56,7 @@ export function Product() {
         <div className="imgProductRight">
           <img src={TigerSystemOrder} />
         </div>
-      </section>
+      </section> */}
     </div>
   );
 }
