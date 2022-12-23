3d065089a73a8e992f4edb0990ed09dd63c1c6e0
Weslley-Prado <weslley.prado73@gmail.com>
2022-12-23 17:15:55 -0300

    feat: create alert message component

--- tigersystemseller_front/tigersystemseller/src/components/index.ts
+++ tigersystemseller_front/tigersystemseller/src/components/index.ts
@@ -2,6 +2,6 @@
  Padrão Barril, semelhante ao usado no Angular
  Evita ficar criando um caminho muito grande na hora de importar
 */
-export { Layout } from "./layout"
-export { RegisterProducts } from "./products"
-export { Input } from "./common"
+export { Layout } from "./layout";
+export { RegisterProducts } from "./products";
+export { Input, Message } from "./common";
