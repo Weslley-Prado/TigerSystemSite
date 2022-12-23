1c8d1cbb23d049ffc591d4761aa7bd9bf6f835be
Weslley-Prado <weslley.prado73@gmail.com>
2022-12-23 16:15:53 -0300

    feat: create function update produtct

--- tigersystemseller-api/src/main/java/com/tigersystemseller/rest/product/ProductController.java
+++ tigersystemseller-api/src/main/java/com/tigersystemseller/rest/product/ProductController.java
@@ -1,8 +1,13 @@
 package com.tigersystemseller.rest.product;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +23,22 @@ public class ProductController {
 	  private ProductRepository repository;
 	
 	  @PostMapping
-      public ProductFormRequest saveProduct(@RequestBody ProductFormRequest product) {
-		  
+      public ProductFormRequest saveProduct(@RequestBody ProductFormRequest product) {		  
 		  Product entityProduct = product.toModel();
 		  repository.save(entityProduct);
 		  product.setId(entityProduct.getId());
     	  return ProductFormRequest.fromModel(entityProduct);
       }
+	  @PutMapping("{id}")
+	  public ResponseEntity<Void> updateProduct(@PathVariable Long id, @RequestBody ProductFormRequest product ) {
+		  Optional<Product> existsProduct = repository.findById(id);
+		  if(existsProduct.isEmpty()) {
+			  return ResponseEntity.notFound().build();
+		  }
+		  Product entityUpdateProduct = product.toModel();
+		  entityUpdateProduct.setId(id);
+		  repository.save(entityUpdateProduct);
+		  
+		  return ResponseEntity.ok().build();
+	  }
 }
