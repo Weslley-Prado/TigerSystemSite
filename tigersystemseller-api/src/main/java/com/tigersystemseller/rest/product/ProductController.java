7c2fe14c46f622e0a24134d2c3f2edfaed7401b5
Weslley-Prado <weslley.prado73@gmail.com>
2023-01-03 10:37:32 -0300

    feat: methods list product

--- tigersystemseller-api/src/main/java/com/tigersystemseller/rest/product/ProductController.java
+++ tigersystemseller-api/src/main/java/com/tigersystemseller/rest/product/ProductController.java
@@ -1,10 +1,13 @@
 package com.tigersystemseller.rest.product;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,26 +22,32 @@ import com.tigersystemseller.model.repository.ProductRepository;
 @RequestMapping("/api/products")
 @CrossOrigin("*")
 public class ProductController {
-	  @Autowired
-	  private ProductRepository repository;
-	
-	  @PostMapping
-      public ProductFormRequest saveProduct(@RequestBody ProductFormRequest product) {		  
-		  Product entityProduct = product.toModel();
-		  repository.save(entityProduct);
-		  product.setId(entityProduct.getId());
-    	  return ProductFormRequest.fromModel(entityProduct);
-      }
-	  @PutMapping("{id}")
-	  public ResponseEntity<Void> updateProduct(@PathVariable Long id, @RequestBody ProductFormRequest product ) {
-		  Optional<Product> existsProduct = repository.findById(id);
-		  if(existsProduct.isEmpty()) {
-			  return ResponseEntity.notFound().build();
-		  }
-		  Product entityUpdateProduct = product.toModel();
-		  entityUpdateProduct.setId(id);
-		  repository.save(entityUpdateProduct);
-		  
-		  return ResponseEntity.ok().build();
-	  }
+	@Autowired
+	private ProductRepository repository;
+	@GetMapping
+	public List<ProductFormRequest> getListProduct(){
+		return repository.findAll().stream()
+				.map(ProductFormRequest::fromModel) // Object of reference, only one parameter;
+				.collect(Collectors.toList());
+	}
+
+	@PostMapping
+	public ProductFormRequest saveProduct(@RequestBody ProductFormRequest product) {		  
+		Product entityProduct = product.toModel();
+		repository.save(entityProduct);
+		product.setId(entityProduct.getId());
+		return ProductFormRequest.fromModel(entityProduct);
+	}
+	@PutMapping("{id}")
+	public ResponseEntity<Void> updateProduct(@PathVariable Long id, @RequestBody ProductFormRequest product ) {
+		Optional<Product> existsProduct = repository.findById(id);
+		if(existsProduct.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		Product entityUpdateProduct = product.toModel();
+		entityUpdateProduct.setId(id);
+		repository.save(entityUpdateProduct);
+
+		return ResponseEntity.ok().build();
+	}
 }
